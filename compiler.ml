59345c09774585d017856fb0916978c90598359b
Eamon Niknafs <en@bu.edu>
2021-12-10 17:06:16 -0500

    Added basic compiler eval() function with LetIn and Add

--- compiler.ml
+++ compiler.ml
@@ -349,7 +349,45 @@ and letrec_parser () =
 let parse_prog (s : string) : (term * char list) option = 
   parse (ws >> term_parser ()) s
   
+(* compiler *)
+let rec eval term out =
+  match term with
+  | LetIn (name, Int term, rest) -> ("Begin Push "^name^" Push "^(string_of_int term)^" Let "^eval rest out^"End ")
+  | LetIn (name, term, rest) -> ("Begin Push "^name^" "^(eval term "")^" Let "^eval rest out^"End ")
+  
+  | Ifgz (t1, t2, t3) -> "Error"
+
+  | Add (Name t1, Name t2) ->(out^" Push "^t1^" Lookup Push "^t2^" Lookup Add ")
+  | Add (t1, Name t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^t2^" Lookup Add ")
+  | Add (Name t1, t2) ->(out^" Push "^t1^" Lookup Push "^(eval t2 "")^" Lookup Add ")
+  | Add (t1, t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^(eval t2 "")^" Lookup Add ")
+
+  | Sub (Name t1, Name t2) ->(out^" Push "^t1^" Lookup Push "^t2^" Lookup Sub")
+  | Sub (t1, Name t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^t2^" Lookup Sub")
+  | Sub (Name t1, t2) ->(out^" Push "^t1^" Lookup Push "^(eval t2 "")^" Lookup Sub")
+  | Sub (t1, t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^(eval t2 "")^" Lookup Sub")
+
+  | Mul (Name t1, Name t2) ->(out^" Push "^t1^" Lookup Push "^t2^" Lookup Mul")
+  | Mul (t1, Name t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^t2^" Lookup Mul")
+  | Mul (Name t1, t2) ->(out^" Push "^t1^" Lookup Push "^(eval t2 "")^" Lookup Mul")
+  | Mul (t1, t2) ->(out^" Push "^(eval t1 "")^" .Lookup Push "^(eval t2 "")^" Lookup Mul")
+
+  | Div (Name t1, Name t2) ->(out^" Push "^t1^" Lookup Push "^t2^" Lookup Div")
+  | Div (t1, Name t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^t2^" Lookup Div")
+  | Div (Name t1, t2) ->(out^" Push "^t1^" Lookup Push "^(eval t2 "")^" Lookup Div")
+  | Div (t1, t2) ->(out^" Push "^(eval t1 "")^" Lookup Push "^(eval t2 "")^" Lookup Div")
+
+  | Trace (term) -> "Error"
+
+  | Int i -> string_of_int i
+
+  | Name n -> (" Push "^n^" Lookup ")
+
+  | _ -> out
+
 (* Compiles a program written in the Part3 Term Language to Part2 Stack Language.
  * Required by the autograder, do not change its type. *)
 let compiler (src : string) : string =
-  failwith "unimplemented"
+  match parse_prog src with
+  | Some (term, []) -> eval term ""
+  | _ -> ("Error")
